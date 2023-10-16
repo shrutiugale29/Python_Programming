5cbb648fe02c1f870597de5d07b2c6f3c9f8d690
Shruti Ugale <131485081+shrutiugale29@users.noreply.github.com>
2023-10-16 13:18:02 +0530

    Add files via upload

---
+++ Pie_chart.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+p=['a','b','c','d','e']
+q=[1,4,9,16,25]
+plt.pie(q, labels=p)
+plt.show()
---
+++ Series_panda.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+info=np.array(['p','q','r'])
+a=pd.Series(info)
+print(a)
