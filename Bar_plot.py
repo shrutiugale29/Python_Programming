cf9b7e523db516919a1c1903cdbe7daba07608e6
Shruti Ugale <131485081+shrutiugale29@users.noreply.github.com>
2023-10-16 13:00:16 +0530

    Add files via upload

---
+++ Bar_plot.py
@@ -0,0 +1,10 @@
+from matplotlib import pyplot as plt
+x=[1,2,3,4,5]
+y=[1,4,9,16,25]
+plt.bar(x,y)
+plt.show()
+plt.barh(x,y)
+plt.show()
+plt.hist(x)
+plt.scatter(x,y)
+plt.show()
---
+++ Line_plot.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+x=[5,2,9,4,7]
+y=[10,5,8,4,2]
+plt.figure(figure=(10,10))
+plt.title("student chart")
+plt.plot(x,y)
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.show()
